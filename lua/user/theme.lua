4c632e6c68d403b9a3aea8fc74a0aa8224c5275e
Jingun Hong <jingun.hong@gmail.com>
2022-05-23 17:53:35 +0900

    add safe setup() for github-theme

--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -1,4 +1,8 @@
-require("github-theme").setup({
-  theme_style = "dimmed",
-  -- other config
+local status_ok, github_theme = pcall(require, "github-theme")
+if not status_ok then
+  return
+end
+
+github_theme.setup({
+  theme_style = "dimmed"
 })
