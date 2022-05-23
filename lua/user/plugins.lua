7daf7cc08a370d20586f9fc1be3c1530971c38dd
Jingun Hong <jingun.hong@gmail.com>
2022-05-23 18:21:58 +0900

    add gitsigns

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -85,6 +85,8 @@ return packer.startup(function(use)
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
 
+  use "lewis6991/gitsigns.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
