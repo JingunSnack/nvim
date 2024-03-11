86acc80b6887162d96b94112aea72b11177c6e3f
Jingun Hong <jingun.hong@sap.com>
2024-03-11 09:00:54 +0900

    use terminal font

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -32,7 +32,6 @@ local options = {
 	wrap = false, -- display lines as one long line
 	scrolloff = 8, -- is one of my fav
 	sidescrolloff = 8,
-	guifont = "CaskaydiaCove Nerd Font Mono:h14", -- the font used in graphical neovim applications
 }
 
 vim.opt.shortmess:append("c")
